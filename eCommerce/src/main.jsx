a25817a7019792577ca3df25567c2e2dc9c381a0
gleschevich <gejo.leschevich@gmail.com>
2022-07-13 15:27:10 -0300

    Intento de Bootstrap SASS y NavBar. Tira error en SASS

--- eCommerce/src/main.jsx
+++ eCommerce/src/main.jsx
@@ -4,7 +4,7 @@ import App from './App'
 import './index.css'
 import  './components/navBar/NavBar'
 import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import './styles.scss'
+//import './styles.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
