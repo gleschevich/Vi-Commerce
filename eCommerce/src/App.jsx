5ded8453b8c47fbe7b7286dfe624881165e5a790
gleschevich <gejo.leschevich@gmail.com>
2022-07-14 13:09:19 -0300

    Hecha la navBar sin diseño

--- eCommerce/src/App.jsx
+++ eCommerce/src/App.jsx
@@ -1,19 +1,15 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import './App.css'
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
-import NavBar from './components/navBar/NavBar';
-
+import '../src/styles.scss'
+import NavBar from './components/navBar/NavBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-     
-    <Router>
       <NavBar/>
-    </Router>
     </div>
   )
 }
--- eCommerce/src/main.jsx
+++ eCommerce/src/main.jsx
@@ -2,9 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
-import  './components/navBar/NavBar'
-import '../../node_modules/bootstrap/dist/css/bootstrap.min.css'
-//import './styles.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
