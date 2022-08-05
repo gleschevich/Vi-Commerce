f076bf0e47339e886b26c3584279e2f30a7ab5db
gleschevich <gejo.leschevich@gmail.com>
2022-08-05 18:19:56 -0300

    Agregada navegacion, solucionado problema de link en detalle

--- eCommerce/src/components/item/Item.jsx
+++ eCommerce/src/components/item/Item.jsx
@@ -10,13 +10,14 @@ const Item = (props) => {
   return (
     <>
     
+    
     <div className='card mt-lg-0 mt-md-0 mt-sm-3 mb-3'>
     <img className='card-img-top border border-danger' src={props.productos.imagenURL}/>
         <div className='card-body'>
             <p className='text-primary' id='text-precio'>${props.productos.precio}</p>
         </div>           
         <div className='mb-3' id='details-icons'>
-          <a><Link className='btn btn-primary' to='/producto/${props.key}' id='btn-detalle'>Ver Detalle</Link> </a>
+          <a><Link className='btn btn-primary' to={`/producto/${props.productos.id}`} id='btn-detalle'>Ver Detalle</Link> </a>
           
           <a className='btn btn-primary btn-icon ms-3' id='fav-icon'> <FaHeart/> </a>
         </div>
